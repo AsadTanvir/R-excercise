dc6e89182455535fe0079c941402d155c1243267
AsadTanvir <asadbuet.757@gmail.com>
2022-05-09 19:46:48 -0400

    Create 1-ggplot.R

---
+++ 1-ggplot.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+
+mpg <- dput(mpg)
+
+
+## ggplots: engine displacement vs highway
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x= displ, y = hwy), color = "red") # scatter-point color is red
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x= displ, y= hwy, color= class)) # 3rd var. "class" shown by colors
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, size=class)) # 3rd var. "class" shown by sizes
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy, shape=class)) # 3rd var. "class" shown by shapes
+
+## ggplots: facets
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y= hwy))+
+  facet_wrap(~class)                          # facet by one var.
+
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y= hwy))+
+  facet_grid(drv~class)                      # facet by multiple var. combination
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y= hwy))+
+  facet_grid(.~class)                      # all in same row for a single var
