b2f9c184090f7062fe7437e78352c7458189dfef
Tanvir <aht17001@ad.uconn.edu>
2022-07-15 23:27:20 -0400

    Update csts_trips.R

--- csts_trips.R
+++ csts_trips.R
@@ -6,7 +6,7 @@ library(dplyr)
 df <- read.csv("FullDataset_LinkedTrips_merged_attributes.csv")   #65103
 
 ## drop few 'columns'
-df <- select(df, -c(linked_tripid,  tourid))
+df <- select(df, -c(linked_tripid,  tourid, primary_mode_category_linked_recode))
 
 ## Rename some long column names to short ones
 colnames(df)
@@ -17,16 +17,22 @@ df <- dplyr::rename(df, o_address=o_address_recode_2_linked,
                                d_lat = d_lat_recode_linked, d_lng = d_lng_recode_linked, 
                                d_county = destination_county_recode_linked, d_town = destination_town_recode_linked, 
                                d_bg = destination_bg_geoid_recode_linked, mode=primary_mode_linked,
-                    mode_category=primary_mode_category_linked)
+                    mode_category=primary_mode_category_linked, dep_time=departure_time_hhmm_linked,
+                    arr_time=arrival_time_hhmm_linked,duration=reported_duration_linked_recode, transit_line=transit_line_linked,
+                    o_addr=o_address_recode_2_linked, d_addr=d_address_recode_linked,transfer=transit_transfers_llinked)
 
 ## subset/filter only the HA inbound trips: transit and Auto [rows = filter()]
 df_HA <- filter(df, o_bg != -999 & d_bg != -999 & d_county == "Hartford County" & 
                   o_county != "New London County" & o_county != "Fairfield County" & 
-                  o_county != "Litchfield County" & o_county != "Windham County")        #16059
-df_HA_transit <- filter(df_HA, mode_category == 6)  #552
+                  o_county != "Litchfield County" & o_county != "Windham County")     #16059
+df_HA_transithh <- filter(df_HA, df_HA$transithh==1)  # HH used transit on travel day = 1351
+
+#df_HA_transit1 <- filter(df_HA, mode_category == 6)  #552
+df_HA_transit <- filter(df_HA_transithh, mode_category==6) #552
 table(df_HA_transit$mode)
-df_HA_transit <- filter(df_HA_transit, mode != 13)  #551
-df_HA_auto <- filter(df_HA, mode_category !=4 & mode_category !=6 & mode_category != 97) #13468
+df_HA_transit <- filter(df_HA_transit, mode != 13)  #551 
+
+df_HA_auto <- filter(df_HA_transithh, mode_category !=4, mode_category !=6, mode_category != 97) #502
 table(df_HA_auto$mode)
 
 ## How many of them have license? 
@@ -36,6 +42,8 @@ table(df_HA_auto$mode)
 
 ## Value counts of OD pairs from transit trips
 df_transOD <- add_count(df_HA_transit, o_bg, d_bg)
+df_transOD <- select(df_transOD,o_bg,d_bg,dep_time,arr_time,duration,n,o_addr,d_addr,transit_line,transfer,everything())
+df_transOD <- arrange(df_transOD, desc(n))
 #df_odcnt <- arrange(df_odcnt, desc(n))    # descending by value of "n"
 df_transOD_uniq <- unique(df_transOD[c('o_bg', 'd_bg', "n")])
 
@@ -43,15 +51,10 @@ df_transOD_uniq <- unique(df_transOD[c('o_bg', 'd_bg', "n")])
 ## extracting auto trips from 'df_HA_auto' by matching OD pairs from 'df_transOD_uniq'
 df_autoOD <- df_HA_auto[df_HA_auto$o_bg %in% df_transOD_uniq$o_bg & 
                           df_HA_auto$d_bg %in% df_transOD_uniq$d_bg,]
-
-
 df_autoOD_uniq <- unique(df_autoOD[c('o_bg', 'd_bg')])
-df_mixed_odcnt <- add_count(df_mixed, o_bg, d_bg)
 
-## counts number of each value in a column in df
-table(df_mixed_odcnt$n)
-#df_mixed_odcnt2 <- filter(df_mixed_odcnt, n>1)
-
-table(df_mixed_odcnt$mode)
-df_mixed_new <- filter(df_mixed_odcnt, mode==8 & mode==9 & mode==11 & mode==12 & mode==14 &
-                         mode==2 & mode==3 & mode==4 & mode==5 & mode==6)
+## Export the data as csv
+write.csv(df_HA_transit, 
+          file = "C:/Users/aht17001/Documents/MEGA/UCONN/CSTS Data/DataDriven_csts/csts_HA_transit.csv", row.names=F)
+write.csv(df_HA_auto, file = "C:/Users/aht17001/Documents/MEGA/UCONN/CSTS Data/DataDriven_csts/csts_HA_auto.csv",
+          row.names = F)
